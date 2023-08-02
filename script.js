b2e5b656d690e5c5e1e14faf9443f840d351abe1
myke <81974207+heismyke@users.noreply.github.com>
2023-08-02 10:49:44 +0100

    first commit

--- script.js
+++ script.js
@@ -1,50 +1,35 @@
-$(document).ready(function() {
 
-    var tasks = " "
+        $(document).ready(function() {
+            var tasks = "";
 
-    $("#add").prop("disabled", true)
+            $("#add").prop("disabled", true);
 
-    $("#taskArea").keyup(function(){
-        var value = $(this).val()
+            $("#taskArea").keyup(function() {
+                var value = $(this).val();
 
-        if(value.length > 0){
-
-            $("#add").prop("disabled", false)
-
-        }else{
-
-            $("#add").prop("disabled", true)
-
-        }
-
-        $("#add").off("click").click( function(){
-
-            tasks = $("#taskArea").val().trim();
-
-            var li = $("<li></li>")
-            
-            li.append(tasks)
-
-
-            $("#unorderedList").append(li)
-
-            var del = $("<i class='fa-solid fa-trash-can'></i>")
-            $("li").append(del)
-
-            
-            del.click(function(){
-                li.remove();
+                if (value.trim().length > 0) {
+                    $("#add").prop("disabled", false);
+                } else {
+                    $("#add").prop("disabled", true);
+                }
             });
 
-            $("#taskArea").val("");
-            
-            $("#add").prop("disabled", true)
+            $("#add").off("click").click(function() {
+                tasks = $("#taskArea").val().trim();
 
-            
-        })
+                var li = $("<li></li>");
+                li.append(tasks);
 
-    })
+                var del = $("<i class='fa-solid fa-trash-can'></i>");
+                li.append(del);
 
-  
+                $("#unorderedList").append(li);
 
-});
+                del.click(function() {
+                    li.remove();
+                });
+
+                $("#taskArea").val("");
+                $("#add").prop("disabled", true);
+            });
+        });
