97913541cb0b6cb4e1e94c20711eafb5892d2e12
myke <81974207+heismyke@users.noreply.github.com>
2023-08-02 02:32:09 +0100

    first commit

--- script.js
+++ script.js
@@ -1,5 +1,38 @@
 $(document).ready(function() {
-   $("#add").click( function() {
-    console.log("Hello!");
-   })
+
+    var tasks = " "
+
+    $("#add").prop("disabled", true)
+
+    $("#taskArea").keyup(function(){
+        var value = $(this).val()
+
+        if(value.length > 0){
+
+            $("#add").prop("disabled", false)
+
+        }else{
+
+            $("#add").prop("disabled", true)
+
+        }
+
+        $("#add").off("click").click( function(){
+
+            tasks = $("#taskArea").val().trim();
+
+            var li = $("<li></li>")
+
+            li.append(tasks)
+
+            $("#unorderedList").append(li)
+
+            $("#taskArea").val("");
+            
+            $("#add").prop("disabled", true)
+        })
+
+    })
+   
+
 });
